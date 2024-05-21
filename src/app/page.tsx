97fbf7813ed1a9887f5a9da1feccf9b1879696b7
tatsuru koike <t-koike@marsflag.com>
2024-05-21 14:23:03 +0900

    change UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import Flow from "@/components/Flow/index";
 import styles from "@/styles/Home.module.scss";
 // import siteTree from "../../site_tree.json";
+import Image from "next/image";
 import "reactflow/dist/style.css";
 import { useState, useEffect } from "react";
 
@@ -41,7 +42,149 @@ const Home: NextPage = () => {
           <button onClick={runCrawl}>Run Crawl</button>
         </div>
       )}
-      {isCrawling && <div className={styles.loading}>Crawling...</div>}
+      {isCrawling && (
+        <div className={styles.loadingWrapper}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <circle cx="12" cy="2" r="0" fill="currentColor">
+              <animate
+                attributeName="r"
+                begin="0"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(45 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.125s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(90 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.25s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(135 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.375s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(180 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.5s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(225 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.625s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(270 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.75s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+            <circle
+              cx="12"
+              cy="2"
+              r="0"
+              fill="currentColor"
+              transform="rotate(315 12 12)"
+            >
+              <animate
+                attributeName="r"
+                begin="0.875s"
+                calcMode="spline"
+                dur="1s"
+                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                repeatCount="indefinite"
+                values="0;2;0;0"
+              />
+            </circle>
+          </svg>
+          <h2 className={styles.loading}>Crawling</h2>
+        </div>
+      )}
+
       {siteTree && <Flow siteTree={siteTree} />}
     </div>
   );
