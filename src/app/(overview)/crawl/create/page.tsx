1e424d58d409b4a1f7aab48cca7b3fcdffadab16
tatsuru koike <t-koike@marsflag.com>
2024-05-30 14:06:07 +0900

    change icon

--- src/app/(overview)/crawl/create/page.tsx
+++ src/app/(overview)/crawl/create/page.tsx
@@ -4,6 +4,7 @@ import Flow from "@/components/ReactFlow/index";
 import "reactflow/dist/style.css";
 import { useState } from "react";
 import clsx from "clsx";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
 const Crawl: NextPage = () => {
@@ -96,143 +97,7 @@ const Crawl: NextPage = () => {
       )}
       {isCrawling && (
         <div className="flex h-full flex-col items-center justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100"
-            height="100"
-            viewBox="0 0 24 24"
-          >
-            <circle cx="12" cy="2" r="0" fill="currentColor">
-              <animate
-                attributeName="r"
-                begin="0"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(45 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.125s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(90 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.25s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(135 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.375s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(180 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.5s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(225 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.625s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(270 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.75s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-            <circle
-              cx="12"
-              cy="2"
-              r="0"
-              fill="currentColor"
-              transform="rotate(315 12 12)"
-            >
-              <animate
-                attributeName="r"
-                begin="0.875s"
-                calcMode="spline"
-                dur="1s"
-                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-                repeatCount="indefinite"
-                values="0;2;0;0"
-              />
-            </circle>
-          </svg>
+          <ArrowPathIcon className="h-20 w-20 animate-spin" />
           <h2 className="text-2xl">Crawling</h2>
         </div>
       )}
--- src/components/Layout/NavLinks.tsx
+++ src/components/Layout/NavLinks.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from "@heroicons/react/24/outline";
+import { HomeIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
