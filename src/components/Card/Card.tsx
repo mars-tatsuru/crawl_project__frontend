9467b6e20e41956c479151d8f1f55a77ba238ef2
tatsuru koike <t-koike@marsflag.com>
2024-05-30 17:22:03 +0900

    change card contents

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 export default function Card() {
   return (
-    <div className="bg-white rounded-md w-64 p-5">
-      <p className="font-bold text-gray-800 text-sm">
-        ここに過去のクロール情報が入って、クリックするとサイトマップに行けたり、、、
-      </p>
+    <div className="w-96 rounded-md border-gray-600 bg-gray-800 p-5">
+      <p className="text-sm font-bold text-white">サムネ</p>
+      <p className="text-sm font-bold text-white">クロール数</p>
+      <p className="text-sm font-bold text-white">クロール開始時間</p>
+      <p className="text-sm font-bold text-white">クロール終了時間</p>
+      <p className="text-sm font-bold text-white">ページ数</p>
+      <p className="text-sm font-bold text-white">site mapへのリンク</p>
     </div>
   );
 }
